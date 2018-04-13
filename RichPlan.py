46167e5c6e2339038c1a11537ab79469bcc749d1
余洪江 <tops@MRJ001.local>
2018-04-13 16:34:13 +0800

    修改爬虫方式

--- RichPlan.py
+++ RichPlan.py
@@ -1,3 +1,4 @@
+#coding:utf-8
 from bs4 import BeautifulSoup
 import urllib.request
 import ssl
@@ -13,16 +14,88 @@ connect = pymysql.Connect(
     charset='utf8'
 )
 
-cur = connect.cursor()
-cur.execute("set charset utf8")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+option = Options()
+option.add_argument("--headless")
+option.add_argument("--disable-gpu")
+browser = webdriver.Chrome(chrome_options=option)
+
+def getMianUrl(receiveUrl):
+    browser.get(receiveUrl)
+
+    try:
+        zhongZiObj = BeautifulSoup(browser.page_source, 'lxml')
+        # print(zhongZiObj)
+
+        for tr in zhongZiObj.find_all('tr', class_='t_tr1'):
+            values = tr.find_all('td')
+            # 期数
+            period = values[0].string
+            # 1-7号码
+            A = values[1].string
+            B = values[2].string
+            C = values[3].string
+            D = values[4].string
+            E = values[5].string
+            F = values[6].string
+            G = values[7].string
+
+            # 所有奖金池里的奖金
+            all_rich = values[9].string
+            # 一等奖人数
+            first_winner = values[10].string
+            # 一等奖奖金
+            first_rich = values[11].string
+            # 二等奖人数
+            second_winner = values[12].string
+            # 二等奖奖金
+            second_rich = values[13].string
+            # 投注总额
+            buy_money = values[14].string
+            # 日期
+            rich_date = values[15].string
+
+            print('********' + period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner,
+                  second_rich,
+                  buy_money, rich_date)
+            print('\n')
+            print(sql, (
+                period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich, buy_money,
+                rich_date))
+            for value in tr.find_all('td', class_='t_cfont2'):
+                print('*******************%s' % value)
+            sql = '''insert into Rich_table VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
+            print(sql, (
+                period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich, buy_money,
+                rich_date))
+            cur.execute(sql, (
+                period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich, buy_money,
+                rich_date))
+            connect.commit()
+    except:
+        print("出现异常")
+    finally:
+        browser.close()
+
+# cur = connect.cursor()
+# cur.execute("set charset utf8")
 
 context = ssl._create_unverified_context()
 url = 'https://datachart.500.com/ssq/history/newinc/history.php?start=00001&end=17126'
 
 def getDataForUrl(url):
-    request = urllib.request.Request(url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.103 Safari/537.36', 'Content-Type': 'text/html;charset=UTF-8'}
+
+    request = urllib.request.Request(url, headers=headers)
+
     response = urllib.request.urlopen(request, context=context)
+    result = response.read()
     resObj = BeautifulSoup(response, 'lxml')
+    print(result.decode("utf8","ignore"))
     for tr in resObj.find_all('tr', class_='t_tr1'):
         values = tr.find_all('td')
         # 期数
@@ -51,22 +124,12 @@ def getDataForUrl(url):
         # 日期
         rich_date = values[15].string
 
-        print('********' + period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich,
-              buy_money, rich_date)
-        print('\n')
-        # for value in tr.find_all('td', class_='t_cfont2'):
-        #     print('*******************%s' % value)
-        sql = '''insert into Rich_table VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'''
-        print(sql, (
-        period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich, buy_money,
-        rich_date))
-        cur.execute(sql, (
-        period, A, B, C, D, E, F, G, all_rich, first_winner, first_rich, second_winner, second_rich, buy_money,
-        rich_date))
-        connect.commit()
 
 
 
-getDataForUrl(url)
+
+getMianUrl(url)
+
+
 cur.close()
 connect.close()
